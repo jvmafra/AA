6101fadb162e89a298e354306ac057ba5d5d9933
jvmafra <joao.mafra@ccc.ufcg.edu.br>
2016-09-03 11:04:37 -0300

    up

---
+++ cartografia.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <string.h>
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+using namespace std;
+ 
+int dist[1000001];
+vector<int> adj[1000001];
+int INF = 1000000000;
+ 
+void zeraDists(int n){
+	for (int i=1; i <= n; i++){
+    	dist[i] = INF;
+    }	
+}
+ 
+int BFS(int vertice){
+	int maior = 0;
+    queue<int> fila;
+    fila.push(vertice);
+    dist[vertice] = 0;
+    int atual;
+ 
+    while(!fila.empty()){
+        atual = fila.front();
+        fila.pop();
+        for (int i=0; i < adj[atual].size(); i++) {
+        	int vizinho = adj[atual][i];
+	        if (dist[vizinho] > dist[atual] + 1) {
+	        	dist[vizinho] = dist[atual] + 1;
+	        	if (dist[vizinho] > maior){
+					maior = dist[vizinho];
+				}
+	        	fila.push(vizinho);
+	        }
+    	} 
+    }
+    
+    return maior;
+ 
+}
+
+int main(){
+	int n, u, v;
+	
+	int aux;
+	int max = 0;
+	
+    scanf("%d", &n);
+ 
+    for (int i=0;i< n-1;i++) {
+        scanf("%d %d", &u, &v);      
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+ 
+    }
+		
+    for (int i = 1; i <= n; i++) {
+		zeraDists(n);
+    	aux = BFS(i);
+    	
+    	if (aux > max) {
+			max = aux;
+		}
+    }
+    printf("%d", max);
+    
+    return 0;
+}
---
+++ dijkstra.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <string.h>
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+using namespace std;
+
+
+int vis[1000001];
+int dist[1000001];
+vector<pair<int,int> > adj[1000001];
+int INF = 100000000;
+
+void zeraDists(int n){
+	for (int i=1; i <= n; i++){
+    	dist[i] = INF;
+    }	
+}
+
+
+int dijkstra(int init, int destino){
+    priority_queue < pair<int,int>, vector<pair<int,int > >, greater<pair<int,int > > >   fila;
+    fila.push(pair<int,int>(0, init));
+    pair<int,int> par;
+    dist[init] = 0;
+
+    while(!fila.empty()){
+        par = fila.top();
+        int u = par.second;
+        fila.pop();
+        
+        if (vis[u] == 0){
+			vis[u] = 1;
+			for (int i=0; i<adj[u].size(); i++) {
+				int v = adj[u][i].second;
+				int custo = adj[u][i].first;
+				if (dist[v] > dist[u] + custo){
+					dist[v] = dist[u] + custo;
+					fila.push(pair<int,int>(dist[v], v));
+				} 
+				
+			} 
+			
+		}
+        
+    }
+    
+    return dist[destino];
+
+}
+
+
+int main() {
+     int n, u, v,m, w;
+    scanf("%d %d", &n, &m);
+    
+    zeraDists(n);
+    
+    for (int i=0;i< m;i++) {
+        scanf("%d %d %d", &u, &v, &w);      
+        adj[u].push_back(pair<int, int>(w, v));
+        
+    }
+    
+    int aux = dijkstra(0, 4);
+    
+    printf ("%d", aux);
+   
+    return 0;
+}
